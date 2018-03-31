2ecc06fb20a18bfbd0b83eea08c806a45acf9a39
Mary Jack <maryjackinthebox@gmail.com>
2018-03-31 00:24:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   
-  def initialize(name)
+  def name(name)
     @name = name
   end
  
@@ -13,7 +13,7 @@ class Person
   end
  
   
-  def initialize(job)
+  def job=(job)
     @job = job
   end
  
