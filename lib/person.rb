4641e6278131e0173aac73f78285e109894a0646
Mary Jack <maryjackinthebox@gmail.com>
2018-03-31 00:25:17 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  
+  def name(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+  
+  def job=(job)
+    @job = job
+  end
+ 
+  def job
+    @job
+  end
+  
+  
+end #Person
+
